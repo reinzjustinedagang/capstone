ee7ac28838684817889171fe3cb5d226e820ab7b
reinzjustinedagang <reinzjustinedagang.bsit@gmail.com>
2025-09-05 09:24:19 +0800

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -64,7 +64,10 @@ app.use(
 // CORS configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173", // local dev
+      process.env.FRONTEND_URL, // production frontend
+    ],
     credentials: true,
   })
 );
