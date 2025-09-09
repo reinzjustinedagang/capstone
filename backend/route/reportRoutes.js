efa8790a2d17b7ee6d67df34c9b4f8fec1e3e1b1
reinzjustinedagang <reinzjustinedagang.bsit@gmail.com>
2025-09-09 20:55:49 +0800

    add charts

--- backend/route/reportRoutes.js
+++ backend/route/reportRoutes.js
@@ -26,7 +26,7 @@ router.get("/gender", async (req, res) => {
 
 router.get("/age", async (req, res) => {
   try {
-    const result = await chartService.getAgeDistribution();
+    const result = await reportService.getAgeDistribution();
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ message: "Failed to fetch age distribution." });
